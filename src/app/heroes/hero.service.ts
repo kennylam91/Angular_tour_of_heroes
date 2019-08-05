1512ab30834358855f35a7189304a52d6a4c1ecf
kennylam91 <phamvanlam1991@gmail.com>
2019-08-05 22:32:42 +0700

    define service with method getHeroes()

--- src/app/heroes/hero.service.ts
+++ src/app/heroes/hero.service.ts
@@ -1,9 +1,15 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HEROES} from '../mock-heroes';
 
 @Injectable({
+  // chung ta khai bao rang service nay nen duoc tao ra tu ung dung injector goc
   providedIn: 'root'
 })
 export class HeroService {
+  getHeroes() {
+    return HEROES;
+  }
 
-  constructor() { }
+  constructor() {
+  }
 }
